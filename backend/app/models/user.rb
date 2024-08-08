c27904e6848ce8bafb4be67e75c16b8442b4dc6b
Saif Ullah Arshad <saifullaharshad999@gmail.com>
2024-08-09 01:24:28 +0500

    initial friendship feature

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
+
+    has_many :friendships
+    has_many :friends, through: :friendships, source: :friend
+
+    def friends
+        User.joins(:friendships).where(friendships: {user_id: id})
+    end
 end
